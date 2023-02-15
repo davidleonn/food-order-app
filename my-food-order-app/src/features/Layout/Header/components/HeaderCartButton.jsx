42017ee5d319b5dfd80cfca922aef1df1903234d
david <davidleon_06@hotmail.com>
2023-02-15 20:15:55 +0100

    Added use effect when add items

--- my-food-order-app/src/features/Layout/Header/components/HeaderCartButton.jsx
+++ my-food-order-app/src/features/Layout/Header/components/HeaderCartButton.jsx
@@ -1,18 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import styles from "../styles/HeaderCartButton.module.css";
 import CartIcon from "../../../Cart/components/CartIcon";
 import CartContext from "../../../../store/cart-context";
 
 function HeaderCartButton(props) {
-  const cartContext = useContext(CartContext);
+  const [buttonIsHighlighted, setButtonisHighlighted] = useState(false);
 
-  const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
+  const cartContext = useContext(CartContext);
+  const { items } = cartContext;
+
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${styles.button} ${
+    buttonIsHighlighted ? styles.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonisHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonisHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
