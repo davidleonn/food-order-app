7dea36d943fb037cb748bb5006052d61de9d1618
david <davidleon_06@hotmail.com>
2023-02-15 19:02:18 +0100

    Added cart logic to add items

--- my-food-order-app/src/features/Cart/components/Cart.jsx
+++ my-food-order-app/src/features/Cart/components/Cart.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import styles from "../styles/Cart.module.css";
 import Modal from "../../UI/components/Modal";
+import CartContext from "../../../store/cart-context";
+import CartItem from "./CartItem";
 
 function Cart(props) {
+  const cartContext = useContext(CartContext);
+
+  const totalAmount = `${cartContext.totalAmount.toFixed(2)}€`;
+  const hasItems = cartContext.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={styles["cart-items"]}>
-      {[{ key: "id", id: "c1", name: "sushi", amount: 2, price: 12.99 }].map(
-        (item) => (
-          <li>{item.name}</li>
-        )
-      )}
+      {cartContext.items.map((item) => (
+        <li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+        </li>
+      ))}
     </ul>
   );
 
@@ -19,13 +36,13 @@ function Cart(props) {
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={styles.actions}>
         <button className={styles["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={styles.button}>Order</button>
+        {hasItems && <button className={styles.button}>Order</button>}
       </div>
     </Modal>
   );
--- my-food-order-app/src/features/Meals/components/MealItem/MealItem.jsx
+++ my-food-order-app/src/features/Meals/components/MealItem/MealItem.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import styles from "../../styles/MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../../store/cart-context";
 
 function MealItem(props) {
+  const myCartContext = useContext(CartContext);
+
   const price = `${props.price.toFixed(2)}€`;
+
+  const addToCartHandler = (amount) => {
+    myCartContext.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={styles.meal}>
@@ -14,7 +26,7 @@ function MealItem(props) {
         <div className={styles.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
